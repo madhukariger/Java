5b6fb0a1308c9d9513006b01afe61e09e80fcb1a
Madhu <madhukariger20@gmail.com>
2025-01-06 22:11:52 +0530

    save

---
+++ src/print_vs_printl.java
@@ -0,0 +1,7 @@
+public class print_vs_printl {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.print("Hello World2");
+        System.out.print("Hello World3");
+    }
+}
